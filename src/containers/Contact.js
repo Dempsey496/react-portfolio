48aa41262dafa97711a5af3bae7eca437bd4362a
Dempsey FP <dempseyfinley@gmail.com>
2021-01-25 16:10:42 -0500

    bootstrap added

---
+++ src/containers/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <h1>This is my ContactPage</h1>
+                
+        );
+    }
+}
+
+export default Contact;
