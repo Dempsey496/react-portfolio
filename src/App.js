0b080df829aee17adf60d6afc42b750075e98697
Dempsey FP <dempseyfinley@gmail.com>
2021-01-26 17:51:59 -0500

    react router created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./containers/Projects";
+import Contact from "./containers/Contact";
 import Home from './containers/Home';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Navbar />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+
+    const location = useLocation();
+
     return (
         <div>
-            
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <Link
+                        to="/home"
+                        className={location.pathname === "/home" ? "nav-link active" : "nav-link"}
+                    >
+                        Home
+        </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/projects"
+                        className={location.pathname === "/projects" ? "nav-link active" : "nav-link"}
+                    >
+                        Projects
+        </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/contact"
+                        className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                    >
+                        Contact
+        </Link>
+                </li>
+            </ul>
         </div>
     );
 };
