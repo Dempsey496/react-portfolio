5e3b955dbaab520c0d1d202669c4013fdb01587a
Dempsey FP <dempseyfinley@gmail.com>
2021-01-28 16:38:27 -0500

    projects.json created

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Route exact path="/" component={Home} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/contact" component={Contact} />
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import projects from '../projects.json';
+
 
 class Projects extends Component {
+    componentDidMount() {
+        console.log(projects[0].image)
+    };
     render() {
         return (
             <h1>This is my ProjectsPage</h1>
